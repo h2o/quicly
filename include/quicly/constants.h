55659fb878391530bd6ab60fde8ddc4ce01b1f0e
Kazuho Oku <kazuhooku@gmail.com>
2019-01-13 12:49:27 +0900

    remove unused definitions

--- include/quicly/constants.h
+++ include/quicly/constants.h
@@ -51,15 +51,6 @@
 #define QUICLY_ERROR_SENDBUF_FULL 0xff03
 #define QUICLY_ERROR_CONNECTION_CLOSED 0xff04
 
-/* application error codes returned by */
-#define QUICLY_STREAM_ERROR_IS_OPEN -1
-#define QUICLY_STREAM_ERROR_FIN_CLOSED -2
-#define QUICLY_STREAM_ERROR_NOT_IN_USE -3
-#define QUICLY_STREAM_ERROR_RESETTING -4
-#define QUICLY_STREAM_ERROR_STOPPED 0
-
-typedef int32_t quicly_stream_error_t;
-
 #define QUICLY_BUILD_ASSERT(condition) ((void)sizeof(char[2 * !!(!__builtin_constant_p(condition) || (condition)) - 1]))
 
 typedef int64_t quicly_stream_id_t;
