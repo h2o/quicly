3e7a03f1fde87c50bcdfef3d2dde912760a4cc1e
Kazuho Oku <kazuhooku@gmail.com>
2019-04-12 09:38:32 +0900

    effective idle timeout is `max(advertised, 3pto)`, not the sum

--- lib/quicly.c
+++ lib/quicly.c
@@ -900,8 +900,8 @@ static void update_idle_timeout(quicly_conn_t *conn, int is_in_receive)
     if (idle_msec == INT64_MAX)
         return;
 
-    conn->idle_timeout.at =
-        now + idle_msec + 3 * quicly_rtt_get_pto(&conn->egress.loss.rtt, conn->super.ctx->transport_params.max_ack_delay);
+    uint32_t three_pto = 3 * quicly_rtt_get_pto(&conn->egress.loss.rtt, conn->super.ctx->transport_params.max_ack_delay);
+    conn->idle_timeout.at = now + (idle_msec > three_pto ? idle_msec : three_pto);
     conn->idle_timeout.should_rearm_on_send = is_in_receive;
 }
 
