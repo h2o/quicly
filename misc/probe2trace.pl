1bc64cf58b3b0a7718518509649a858690eee494
Kazuho Oku <kazuhooku@gmail.com>
2019-09-15 14:26:16 +0900

    emit the address of unknown pointer types (avoids "%p", as the output form is platform-dependent)

--- misc/probe2trace.pl
+++ misc/probe2trace.pl
@@ -142,8 +142,10 @@ for my $probe (@probes) {
                 } else {
                     push @ap, "arg$i";
                 }
-            } elsif ($type =~ /^const\s+void\s+\*$/) {
-                # skip const void *
+            } elsif ($type =~ /\s+\*$/) {
+                # emit the address for other pointers
+                push @fmt, qq!"name":"0x%llx"!;
+                push @ap, "(unsigned long long)arg$i";
             } else {
                 die "can't handle type: $type";
             }
