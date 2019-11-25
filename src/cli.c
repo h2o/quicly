480d3b568ab33b4dd665c08b5ac6c29338185a22
Frederik Deweerdt <fdeweerdt@fastly.com>
2019-11-25 14:42:39 -0800

    Amends c2b9bf410861b9906394503a60bcb1e43bd053bc to prevent a buffer overrun

--- src/cli.c
+++ src/cli.c
@@ -138,7 +138,7 @@ static int parse_request(ptls_iovec_t input, char **path, int *is_http1)
 {
     size_t off = 0, path_start;
     
-    printf("Request received: %s\n", input.base);
+    printf("Request received: %.*s\n", (int)input.len, input.base);
     for (off = 0; off != input.len; ++off)
         if (input.base[off] == ' ')
             goto EndOfMethod;
