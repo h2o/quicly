fcbbfd9c75bae8d18ac43c9b2ff918aa873d353d
Sandra O'Keefe <sokeefe@fastly.com>
2020-03-25 16:08:04 -0230

    Fix issue with address token length being undefined.

--- fuzz/decode_receive.cc
+++ fuzz/decode_receive.cc
@@ -10,17 +10,39 @@ void __sanitizer_cov_trace_pc(void)
 {
 }
 
+static struct {
+    ptls_iovec_t tls_ticket;
+    ptls_iovec_t address_token;
+} session_info;
+
+int save_session_ticket_cb(ptls_save_ticket_t *_self, ptls_t *tls, ptls_iovec_t src)
+{
+    free(session_info.tls_ticket.base);
+    session_info.tls_ticket = ptls_iovec_init(malloc(src.len), src.len);
+    memcpy(session_info.tls_ticket.base, src.base, src.len);
+
+    quicly_conn_t *conn = (quicly_conn_t *) *ptls_get_data_ptr(tls);
+    return 0; 
+}
+
+static int on_client_hello_cb(ptls_on_client_hello_t *_self, ptls_t *tls, ptls_on_client_hello_parameters_t *params)
+{
+    return 0;
+}
+
 static ptls_key_exchange_algorithm_t *key_exchanges[128];
 static ptls_cipher_suite_t *cipher_suites[128];
+
+static ptls_save_ticket_t save_session_ticket = {save_session_ticket_cb};
+static ptls_on_client_hello_t on_client_hello = {on_client_hello_cb};
 
 static ptls_context_t tlsctx = {.random_bytes = ptls_openssl_random_bytes,
                                 .get_time = &ptls_get_time,
                                 .key_exchanges = key_exchanges,
                                 .cipher_suites = cipher_suites,
-                                .require_dhe_on_psk = 1 //, HERE IS A COMMA
-                                //.save_ticket = &save_session_ticket,
-                                //.on_client_hello = &on_client_hello};
-				};
+                                .require_dhe_on_psk = 1 , 
+                                .save_ticket = &save_session_ticket,
+                                .on_client_hello = &on_client_hello};
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 {
@@ -40,7 +62,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 	quicly_cid_plaintext_t next_cid;
 	const char* host = "127.0.0.1";
 	const char* port = "4422";
-	ptls_iovec_t resumption_token;
+	ptls_iovec_t *resumption_token = (ptls_iovec_t *)malloc(sizeof(ptls_iovec_t));
 	ptls_handshake_properties_t hs_properties;
 	quicly_transport_parameters_t resumed_transport_params;
 
@@ -55,7 +77,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 
 	memcpy(&sa, res->ai_addr, res->ai_addrlen);
 
-	ret = quicly_connect(&conn, &ctx, host, &sa, NULL, &next_cid, resumption_token, &hs_properties, &resumed_transport_params);
+	
+	resumption_token->len = 0;
+
+	ret = quicly_connect(&conn, &ctx, host, &sa, NULL, &next_cid, *resumption_token, &hs_properties, &resumed_transport_params);
 
 	ret = quicly_decode_packet(&ctx, &p, Data, Size);
 
