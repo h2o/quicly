b821bb57c5da77b4b4fc52048e1eb1fe92b2b0ef
Hajime Fujita <crisp.fujita@nifty.com>
2020-04-23 16:32:46 +0000

    Introduce received CID set
    This patch introduces data structures and functions to manage
    active connection IDs received from the peer.
    This is the first step to separate frame processing and state processing
    to clean the code structure and make it easier to implement unit tests.

--- include/quicly/cid.h
+++ include/quicly/cid.h
@@ -32,14 +32,71 @@ extern "C" {
 #include "quicly/constants.h"
 
 typedef struct st_quicly_cid_t quicly_cid_t;
+typedef struct st_quicly_received_cid_t quicly_received_cid_t;
+typedef struct st_quicly_received_cid_set_t quicly_received_cid_set_t;
 
 struct st_quicly_cid_t {
     uint8_t cid[QUICLY_MAX_CID_LEN_V1];
     uint8_t len;
 };
 
+/**
+ * records a CID given by the peer
+ */
+struct st_quicly_received_cid_t {
+    /**
+     * indicates whether this record holds an active (given by peer and not retired) CID
+     */
+    int is_active;
+    /**
+     * sequence number of the CID
+     *
+     * If is_active, this represents the sequence number associated with the CID.
+     * If !is_active, this represents a "reserved" slot, meaning that we are expecting to receive a NEW_CONNECTION_ID frame
+     * with this sequence number. This helps determine if a received frame is carrying a CID that is already retired.
+     */
+    uint64_t sequence;
+    struct st_quicly_cid_t cid;
+    uint8_t stateless_reset_token[QUICLY_STATELESS_RESET_TOKEN_LEN];
+};
+
+/**
+ * structure to hold active connection IDs received from the peer
+ */
+struct st_quicly_received_cid_set_t {
+    /**
+     * we retain QUICLY_LOCAL_ACTIVE_CONNECTION_ID_LIMIT active connection IDs
+     * cids[0] holds the current (in use) CID which is used when emitting packets
+     */
+    struct st_quicly_received_cid_t cids[QUICLY_LOCAL_ACTIVE_CONNECTION_ID_LIMIT];
+    /**
+     * we expect to receive CIDs with sequence number smaller than or equal to this number
+     */
+    uint64_t _largest_sequence_expected;
+};
+
 static void quicly_set_cid(quicly_cid_t *dest, ptls_iovec_t src);
 static int quicly_cid_is_equal(const quicly_cid_t *cid, ptls_iovec_t vec);
+void quicly_received_cid_init(struct st_quicly_received_cid_set_t *set);
+/**
+ * registers received connection ID
+ * returns 0 if successfully (registered or ignored because of duplication/stale information), transport error code otherwise
+ */
+int quicly_received_cid_register(struct st_quicly_received_cid_set_t *set, uint64_t sequence, const uint8_t *cid, size_t cid_len,
+                                 const uint8_t srt[QUICLY_STATELESS_RESET_TOKEN_LEN]);
+/**
+ * unregisters specified CID from the store
+ * returns 0 if success, 1 if failure
+ */
+int quicly_received_cid_unregister(struct st_quicly_received_cid_set_t *set, uint64_t sequence);
+/**
+ * unregister all CIDs with sequence number smaller than seq_unreg_prior_to
+ *
+ * @param unregistered_seqs sequence numbers of unregistered CIDs are returned, packed from the begining of the array
+ * @return the number of unregistered CIDs
+ */
+size_t quicly_received_cid_unregister_prior_to(struct st_quicly_received_cid_set_t *set, uint64_t seq_unreg_prior_to,
+                                               uint64_t unregistered_seqs[QUICLY_LOCAL_ACTIVE_CONNECTION_ID_LIMIT]);
 
 inline int quicly_cid_is_equal(const quicly_cid_t *cid, ptls_iovec_t vec)
 {
--- lib/cid.c
+++ lib/cid.c
@@ -22,3 +22,140 @@
 
 #include <string.h>
 #include "quicly/cid.h"
+
+void quicly_received_cid_init(struct st_quicly_received_cid_set_t *set)
+{
+    memset(set, 0, sizeof(*set));
+    for (size_t i = 0; i < PTLS_ELEMENTSOF(set->cids); i++) {
+        set->cids[i].sequence = i;
+    }
+    set->_largest_sequence_expected = PTLS_ELEMENTSOF(set->cids) - 1;
+}
+
+/**
+ * promote CID at idx_to_promote as the current CID for communication
+ * i.e. swap cids[idx_to_promote] and cids[0]
+ */
+static void promote_cid(struct st_quicly_received_cid_set_t *set, size_t idx_to_promote)
+{
+    uint64_t seq_tmp = set->cids[0].sequence;
+
+    assert(idx_to_promote > 0);
+    assert(!set->cids[0].is_active);
+
+    set->cids[0] = set->cids[idx_to_promote];
+    set->cids[idx_to_promote].is_active = 0;
+    set->cids[idx_to_promote].sequence = seq_tmp;
+}
+
+int quicly_received_cid_register(struct st_quicly_received_cid_set_t *set, uint64_t sequence, const uint8_t *cid, size_t cid_len,
+                                 const uint8_t srt[QUICLY_STATELESS_RESET_TOKEN_LEN])
+{
+    int was_stored = 0;
+
+    if (set->_largest_sequence_expected < sequence)
+        return QUICLY_TRANSPORT_ERROR_CONNECTION_ID_LIMIT;
+
+    for (size_t i = 0; i < PTLS_ELEMENTSOF(set->cids); i++) {
+        if (set->cids[i].is_active) {
+            /* compare newly received CID against what we already have, to see if there is duplication/conflicts */
+
+            /* If an endpoint receives a NEW_CONNECTION_ID frame that repeats a previously issued connection ID with
+             * a different Stateless Reset Token or a different sequence number, or if a sequence number is used for
+             * different connection IDs, the endpoint MAY treat that receipt as a connection error of type PROTOCOL_VIOLATION.
+             * (19.15)
+             */
+            if (quicly_cid_is_equal(&set->cids[i].cid, ptls_iovec_init(cid, cid_len))) {
+                if (set->cids[i].sequence == sequence &&
+                    memcmp(set->cids[i].stateless_reset_token, srt, QUICLY_STATELESS_RESET_TOKEN_LEN) == 0) {
+                    /* likely a duplicate due to retransmission */
+                    return 0;
+                } else {
+                    /* received a frame that carries conflicting information */
+                    return QUICLY_TRANSPORT_ERROR_PROTOCOL_VIOLATION;
+                }
+            }
+            /* here we know CID is not equal */
+            if (set->cids[i].sequence == sequence)
+                return QUICLY_TRANSPORT_ERROR_PROTOCOL_VIOLATION;
+        } else if (!was_stored && set->cids[i].sequence == sequence) {
+            set->cids[i].sequence = sequence;
+            quicly_set_cid(&set->cids[i].cid, ptls_iovec_init(cid, cid_len));
+            memcpy(set->cids[i].stateless_reset_token, srt, QUICLY_STATELESS_RESET_TOKEN_LEN);
+            set->cids[i].is_active = 1;
+            was_stored = 1;
+            if (i > 0 && !set->cids[0].is_active) {
+                /* promote this CID for communication */
+                promote_cid(set, i);
+            }
+        }
+    }
+
+    /* execution reaches here in two cases, 1) normal path, i.e. new CID was successfully registered, and 2) new CID was already
+     * retired (was_stored == 0). */
+    return 0;
+}
+
+static void do_unregister(struct st_quicly_received_cid_set_t *set, size_t idx_to_unreg)
+{
+    assert(set->cids[idx_to_unreg].is_active);
+
+    set->cids[idx_to_unreg].is_active = 0;
+    set->cids[idx_to_unreg].sequence = ++set->_largest_sequence_expected;
+}
+
+int quicly_received_cid_unregister(struct st_quicly_received_cid_set_t *set, uint64_t sequence)
+{
+    uint64_t min_seq = UINT64_MAX, min_seq_idx = UINT64_MAX;
+    for (size_t i = 0; i < PTLS_ELEMENTSOF(set->cids); i++) {
+        if (sequence == set->cids[i].sequence) {
+            do_unregister(set, i);
+            if (i != 0)
+                return 0; /* if not retiring idx=0 (current in-use CID), simply return */
+        }
+        if (set->cids[i].is_active && min_seq > set->cids[i].sequence) {
+            /* find a CID with minimum sequence number, while iterating over the array */
+            min_seq = set->cids[i].sequence;
+            min_seq_idx = i;
+        }
+    }
+
+    if (!set->cids[0].is_active) {
+        /* we have retired the current CID (idx=0) */
+        if (min_seq_idx != UINT64_MAX)
+            promote_cid(set, min_seq_idx);
+        return 0;
+    } else {
+        /* we did not unregister any slot */
+        return 1;
+    }
+}
+
+size_t quicly_received_cid_unregister_prior_to(struct st_quicly_received_cid_set_t *set, uint64_t seq_unreg_prior_to,
+                                               uint64_t unregistered_seqs[QUICLY_LOCAL_ACTIVE_CONNECTION_ID_LIMIT])
+{
+    uint64_t min_seq = UINT64_MAX, min_seq_idx = UINT64_MAX;
+    size_t num_unregistered = 0;
+    for (size_t i = 0; i < PTLS_ELEMENTSOF(set->cids); i++) {
+        if (set->cids[i].is_active) {
+            if (set->cids[i].sequence < seq_unreg_prior_to) {
+                unregistered_seqs[num_unregistered++] = set->cids[i].sequence;
+                do_unregister(set, i);
+                continue;
+            }
+            if (min_seq > set->cids[i].sequence) {
+                /* find a CID with minimum sequence number, while iterating over the array */
+                min_seq = set->cids[i].sequence;
+                min_seq_idx = i;
+            }
+        }
+    }
+
+    if (!set->cids[0].is_active) {
+        /* we have retired the current CID (idx=0) */
+        if (min_seq_idx != UINT64_MAX)
+            promote_cid(set, min_seq_idx);
+    }
+
+    return num_unregistered;
+}
