2dcef616b522903ecc2522664927bea89f9f9f13
Toru Maesaka <toru@torumk.com>
2020-12-01 17:25:40 -0800

    qlog: remove redundant ack rendering code

--- misc/qlog-adapter.py
+++ misc/qlog-adapter.py
@@ -72,10 +72,7 @@ def handle_packet_sent(events, idx):
     }]
 
 def handle_ack_send(event):
-    return {
-        "frame_type": "ack",
-        "acked_ranges": [[event["largest-acked"]]]
-    }
+    return render_ack_frame([[event["largest-acked"]]])
 
 def handle_data_blocked_receive(event):
     return {
