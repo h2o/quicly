749b378ae656c9d1757011a52e13a4c8981c143f
Kazuho Oku <kazuhooku@gmail.com>
2021-10-04 18:58:55 +0900

    fix CID overlap - otherwise it is hard to use trace

--- t/simulator.c
+++ t/simulator.c
@@ -598,7 +598,8 @@ int main(int argc, char **argv)
             net_endpoint_init(client_node);
             client_node->start_at = now + start;
             int ret = quicly_connect(&client_node->conns[0].quic, &quicctx, "hello.example.com", &server_node.node.addr.sa,
-                                     &client_node->addr.sa, NULL, ptls_iovec_init(NULL, 0), NULL, NULL);
+                                     &client_node->addr.sa, &next_quic_cid, ptls_iovec_init(NULL, 0), NULL, NULL);
+            ++next_quic_cid.master_id;
             assert(ret == 0);
             quicly_stream_t *stream;
             ret = quicly_open_stream(client_node->conns[0].quic, &stream, 1);
