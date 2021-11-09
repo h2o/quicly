cc78ce341a1374ebd038abff5fdd008fc840cad6
Goro Fuji <goro@fastly.com>
2021-11-09 05:55:56 +0000

    add tests to make sure quicly_set_cc() sets the specified one

--- t/set_cc.c
+++ t/set_cc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017 Fastly, Kazuho Oku
+ * Copyright (c) 2021 Fastly, Kazuho Oku, Goro Fuji.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
