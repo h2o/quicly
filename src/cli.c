c046b6029de9ad6be57b5b7a846c1d7bd26eadd2
Kazuho Oku <kazuhooku@gmail.com>
2023-06-28 10:15:47 +0900

    now that we call `new_socket` there is no nee to set O_NONBLOCK separately

--- src/cli.c
+++ src/cli.c
@@ -750,7 +750,6 @@ static int run_client(int fd, struct sockaddr *sa, const char *host)
                 int newfd = new_socket(local.sa.sa_family);
                 if (newfd != -1) {
                     close(fd);
-                    fcntl(newfd, F_SETFL, O_NONBLOCK);
                     fd = newfd;
                 }
             }
