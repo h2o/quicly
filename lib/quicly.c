4d486d0e59b402146509d695c1dac3946f3b7485
Kazuho Oku <kazuhooku@gmail.com>
2023-11-11 03:57:37 +0100

    refactor, adding probes to mark enter to / exit of cc-limited state

--- lib/quicly.c
+++ lib/quicly.c
@@ -1376,6 +1376,21 @@ static void update_send_alarm(quicly_conn_t *conn, int can_send_stream_data, int
                              can_send_stream_data, handshake_is_in_progress, conn->egress.max_data.sent, is_after_send);
 }
 
+static void update_cc_limited(quicly_conn_t *conn, int is_cc_limited)
+{
+    if (quicly_ratemeter_is_cc_limited(&conn->egress.ratemeter) != is_cc_limited) {
+        if (is_cc_limited) {
+            quicly_ratemeter_enter_cc_limited(&conn->egress.ratemeter, conn->egress.packet_number);
+            QUICLY_PROBE(ENTER_CC_LIMITED, conn, conn->stash.now, conn->egress.packet_number);
+            QUICLY_LOG_CONN(enter_cc_limited, conn, { PTLS_LOG_ELEMENT_UNSIGNED(pn, conn->egress.packet_number); });
+        } else {
+            quicly_ratemeter_exit_cc_limited(&conn->egress.ratemeter, conn->egress.packet_number);
+            QUICLY_PROBE(EXIT_CC_LIMITED, conn, conn->stash.now, conn->egress.packet_number);
+            QUICLY_LOG_CONN(exit_cc_limited, conn, { PTLS_LOG_ELEMENT_UNSIGNED(pn, conn->egress.packet_number); });
+        }
+    }
+}
+
 /**
  * Updates the send alarm and adjusts the delivery rate estimator. This function is called from the receive path. From the sendp
  * path, `update_send_alarm` is called directly.
@@ -1388,7 +1403,7 @@ static void setup_next_send(quicly_conn_t *conn)
 
     /* When the flow becomes application-limited due to receiving some information, stop collecting delivery rate samples. */
     if (!can_send_stream_data)
-        quicly_ratemeter_not_cc_limited(&conn->egress.ratemeter, conn->egress.packet_number);
+        update_cc_limited(conn, 0);
 }
 
 static int create_handshake_flow(quicly_conn_t *conn, size_t epoch)
@@ -4897,14 +4912,10 @@ Exit:
             conn->egress.send_ack_at = INT64_MAX; /* we have sent ACKs for every epoch (or before address validation) */
         int can_send_stream_data = scheduler_can_send(conn);
         update_send_alarm(conn, can_send_stream_data, 1);
-        if (can_send_stream_data &&
-            (s->num_datagrams == s->max_datagrams || conn->egress.loss.sentmap.bytes_in_flight >= conn->egress.cc.cwnd ||
-             pacer_can_send_at(conn) > conn->stash.now)) {
-            /* as the flow is CWND-limited, start delivery rate estimator */
-            quicly_ratemeter_in_cc_limited(&conn->egress.ratemeter, s->first_packet_number);
-        } else {
-            quicly_ratemeter_not_cc_limited(&conn->egress.ratemeter, conn->egress.packet_number);
-        }
+        int is_cc_limited = can_send_stream_data && (s->num_datagrams == s->max_datagrams ||
+                                                     conn->egress.loss.sentmap.bytes_in_flight >= conn->egress.cc.cwnd ||
+                                                     pacer_can_send_at(conn) > conn->stash.now);
+        update_cc_limited(conn, is_cc_limited);
         if (s->num_datagrams != 0)
             update_idle_timeout(conn, 0);
     }
