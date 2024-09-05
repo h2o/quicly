ab91b16697ff6aa47f46e798c5e4c067998d9c51
Kazuho Oku <kazuhooku@gmail.com>
2024-09-05 11:52:28 +0900

    send PATH_RESPONSE if scheduled, otherwise we enter a hot loop unless PATH_CHALLENGE is also scheduled

--- lib/quicly.c
+++ lib/quicly.c
@@ -5553,7 +5553,8 @@ int quicly_send(quicly_conn_t *conn, quicly_address_t *dest, quicly_address_t *s
      * more than one path at a time) */
     if (conn->egress.send_probe_at <= conn->stash.now) {
         for (s.path_index = 1; s.path_index < PTLS_ELEMENTSOF(conn->paths); ++s.path_index) {
-            if (conn->paths[s.path_index] == NULL || conn->stash.now < conn->paths[s.path_index]->path_challenge.send_at)
+            if (conn->paths[s.path_index] == NULL || !(conn->stash.now >= conn->paths[s.path_index]->path_challenge.send_at ||
+                                                       conn->paths[s.path_index]->path_response.send_))
                 continue;
             if (conn->paths[s.path_index]->path_challenge.num_sent > conn->super.ctx->max_probe_packets) {
                 delete_path(conn, s.path_index);
