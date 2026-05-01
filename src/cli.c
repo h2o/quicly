36a364bb175ed60194894af09a5dec7509a64235
Kazuho Oku <kazuhooku@gmail.com>
2026-05-01 14:36:39 +0900

    [cli] CID length check should better be done in quicly_decode_packet

--- src/cli.c
+++ src/cli.c
@@ -955,9 +955,6 @@ static int run_server(int fd, struct sockaddr *sa, socklen_t salen)
                             send_one_packet(fd, &remote, &local, payload, payload_len);
                             break;
                         }
-                        /* there is no way to send response to these v1 packets */
-                        if (packet.cid.dest.encrypted.len > QUICLY_MAX_CID_LEN_V1 || packet.cid.src.len > QUICLY_MAX_CID_LEN_V1)
-                            break;
                     }
 
                     quicly_conn_t *conn = NULL;
